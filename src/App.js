1bcb8f7cd940abde5a28fe00db6c9c9e4e310081
20230656 <seri001@ess.ais.ac.nz>
2024-07-21 17:59:44 +1200

    register link front

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import NumberSumUp from "./components/NumberSumUp";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
+
 function App() {
   return (
     <div className="App">
-        <TopBar/>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/chatroom" element={<ChatRoom/>}/>
-          <Route path="/sumupnum" element={<NumberSumUp/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-      </Routes>
+        <TopBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chatroom" element={<ChatRoom />} />
+            <Route path="/number-sum" element={<NumberSumUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
     </div>
   );
 }
--- src/consistents.js
+++ src/consistents.js
@@ -1,2 +1,3 @@
 // export const BaseUrl= "https://message-nu-eight.vercel.app/"
-export const BaseUrl= "http://localhost:8000/"
+// export const BaseUrl= "http://localhost:8000/"
+export const BaseUrl = "https://message-sigma-nine.vercel.app/";
