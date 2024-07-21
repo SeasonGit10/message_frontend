e96d8a0e002daf44494b6d8758cb4ca999965200
20230656 <seri001@ess.ais.ac.nz>
2024-07-21 18:27:34 +1200

    registeration link

--- src/consistents.js
+++ src/consistents.js
@@ -1,3 +1,3 @@
 // export const BaseUrl= "https://message-nu-eight.vercel.app/"
 // export const BaseUrl= "http://localhost:8000/"
-export const BaseUrl = "https://message-sigma-nine.vercel.app/";
+export const BaseUrl = "https://message-sigma-nine.vercel.app/"
